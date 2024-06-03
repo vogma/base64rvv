285b1580177e45069a67ff7c346e3e5c107df2af
Marco <vogel.marco@mailbox.org>
2024-06-03 21:16:43 +0200

    refactoring

--- Makefile
+++ Makefile
@@ -1,50 +1,32 @@
 SRC=src
 BUILD=build
-INCLUDE=-Iinclude -Ilibb64/include
+INCLUDE=-Iinclude -Ilibb64/include -Ilib/include
 LINK_LIB64=-Llibb64 -lb64
+LINK_LIB_RVV=-Llib/build -lb64rvv
 FLAGS=-march=rv64gcvzba -mabi=lp64d -O3 -static -Wall -g
-CLANG_FLAGS=-O3 -march=rv64gcv -g -c
 
 CC = riscv64-unknown-linux-gnu-gcc
 AR = riscv64-unknown-linux-gnu-ar
-CFLAGS = -Wall -O2
 ARFLAGS = rcs
 
-SOURCES = $(SRC)/base64_rvv_decode.c $(SRC)/base64_rvv_encode.c
 OBJECTS = $(BUILD)/base64_rvv_decode.o $(BUILD)/base64_rvv_encode.o
 LIBARY=libb64rvv.a
 
-base64: Makefile
-	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(SRC)/$@.c $(SRC)/$@.S -o $(BUILD)/$@
-
-base64_decode.o: Makefile
-	clang $(CLANG_FLAGS) $(SRC)/base64_decode.c -o $(BUILD)/$@ $(INCLUDE)
-
-base64_decode: base64_decode.o Makefile
-	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(BUILD)/$@.o -Llibb64 -lb64 -o $(BUILD)/$@
-
-base64_decode_gcc: Makefile
-	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(SRC)/base64_decode.c -o $(BUILD)/base64_decode
-
-cycleCount: Makefile
-	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(SRC)/$@.c $(SRC)/base64.S -o $(BUILD)/$@
-
 base64_rvv_decode.o : lib/$(SRC)/base64_rvv_decode.c 
-	$(CC) $(FLAGS) -Ilib/include -c $< -o lib/$(BUILD)/$@
+	$(CC) $(FLAGS) $(INCLUDE) -c $< -o lib/$(BUILD)/$@
 
 base64_rvv_encode.o : lib/$(SRC)/base64_rvv_encode.c 
-	$(CC) $(FLAGS) -Ilib/include -c $< -o lib/$(BUILD)/$@
+	$(CC) $(FLAGS) $(INCLUDE) -c $< -o lib/$(BUILD)/$@
 
 $(LIBARY): base64_rvv_decode.o base64_rvv_encode.o
 	$(AR) $(ARFLAGS) lib/$(BUILD)/$@  lib/$(BUILD)/base64_rvv_decode.o lib/$(BUILD)/base64_rvv_encode.o
 
-test.o: $(SRC)/test.c
-	$(CC) $(FLAGS) -Ilib/include -c $< -o $(BUILD)/$@
+base64.o: $(SRC)/base64.c
+	$(CC) $(FLAGS) $(INCLUDE) -c $< -o $(BUILD)/$@
 
-test: $(LIBARY) test.o 
-	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(BUILD)/test.o -o $(BUILD)/$@ -Llib/build -lb64rvv
-
+base64: $(LIBARY) base64.o
+	$(RISCV)/riscv64-unknown-linux-gnu-gcc $(FLAGS) $(BUILD)/base64.o -o $(BUILD)/$@ $(LINK_LIB_RVV)
 
 clean: Makefile
 	rm -f $(BUILD)/$(LIBARY)
-	rm -f $(BUILD)/$(OBJECTS)
+	rm -f $(OBJECTS)
--- src/base64.c
+++ src/base64.c
@@ -1,177 +1,13 @@
-#include "base64.h"
+#include <libb64rvv.h>
 
-const unsigned char b64chars[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-int Base64encode_len(int len)
-{
-    return ((len + 2) / 3 * 4) + 1;
-}
-
-int Base64encode(char *encoded, const char *string, int len)
-{
-    int i;
-    char *p;
-
-    p = encoded;
-    for (i = 0; i < len - 2; i += 3)
-    {
-        *p++ = b64chars[(string[i] >> 2) & 0x3F];
-        *p++ = b64chars[((string[i] & 0x3) << 4) |
-                        ((int)(string[i + 1] & 0xF0) >> 4)];
-        *p++ = b64chars[((string[i + 1] & 0xF) << 2) |
-                        ((int)(string[i + 2] & 0xC0) >> 6)];
-        *p++ = b64chars[string[i + 2] & 0x3F];
-    }
-    if (i < len)
-    {
-        *p++ = b64chars[(string[i] >> 2) & 0x3F];
-        if (i == (len - 1))
-        {
-            *p++ = b64chars[((string[i] & 0x3) << 4)];
-            *p++ = '=';
-        }
-        else
-        {
-            *p++ = b64chars[((string[i] & 0x3) << 4) |
-                            ((int)(string[i + 1] & 0xF0) >> 4)];
-            *p++ = b64chars[((string[i + 1] & 0xF) << 2)];
-        }
-        *p++ = '=';
-    }
-
-    *p++ = '\0';
-    return p - encoded;
-}
-
-const uint16_t gather_index_lmul4[] = {1, 0, 2, 1, 4, 3, 5, 4, 7, 6, 8, 7, 10, 9, 11, 10, 13, 12, 14, 13, 16, 15, 17, 16, 19, 18, 20, 19, 22, 21, 23, 22, 25, 24, 26, 25, 28, 27, 29, 28, 31, 30, 32, 31, 34, 33, 35, 34, 37, 36, 38, 37, 40, 39, 41, 40, 43, 42, 44, 43, 46, 45, 47, 46};
-
-vuint16m2_t loadIndex()
-{
-    size_t vl = __riscv_vsetvlmax_e16m2();
-    return __riscv_vle16_v_u16m2(gather_index_lmul4, vl);
-    // return __riscv_vle8_v_u8m1(gather_index, vl);
-}
-
-const int8_t offsets[16] = {65, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -19, -16, 71, 0, 0};
-
-vuint8m1_t __attribute__((always_inline)) inline branchless_table_lookup_opt(vuint8m1_t vec_indices, vint8m1_t offset_vec, size_t vl)
-{
-    vuint8m1_t result = __riscv_vssubu_vx_u8m1(vec_indices, 51, vl);
-
-    vbool8_t ge_26_mask = __riscv_vmsgtu_vx_u8m1_b8(vec_indices, 25, vl);
-    vbool8_t le_51_mask = __riscv_vmsleu_vx_u8m1_b8(vec_indices, 51, vl);
-
-    vbool8_t mask = __riscv_vmand_mm_b8(ge_26_mask, le_51_mask, vl);
-
-    result = __riscv_vmerge_vxm_u8m1(result, 13, mask, vl);
-
-    offset_vec = __riscv_vrgather_vv_i8m1(offset_vec, result, vl);
-
-    vint8m1_t ascii_vec = __riscv_vadd_vv_i8m1(__riscv_vreinterpret_v_u8m1_i8m1(vec_indices), offset_vec, vl);
-
-    return __riscv_vreinterpret_v_i8m1_u8m1(ascii_vec);
-    // return vec_indices;
-}
-
-vuint32m1_t __attribute__((always_inline)) inline create_lookup_indices_opt(vuint8m1_t data, size_t vl)
-{
-
-    vuint32m1_t const_vec_ac = __riscv_vmv_v_x_u32m1(0x04000040, vl);
-    vuint32m1_t const_vec_bd = __riscv_vmv_v_x_u32m1(0x01000010, vl);
-
-    vuint32m1_t input32 = __riscv_vreinterpret_v_u8m1_u32m1(data);
-
-    // mask out so that only a and c bits remain
-    vuint32m1_t index_a_c = __riscv_vand_vx_u32m1(input32, 0x0FC0FC00, vl);
-
-    // mask out so that only a and c bits remain
-    vuint32m1_t index_b_d = __riscv_vand_vx_u32m1(input32, 0x003F03F0, vl);
-
-    vl = __riscv_vsetvlmax_e16m1();
-    // multiply 16-bit integers and store high 16 bits of 32-bit result
-    vuint16m1_t vec_shifted_ac = __riscv_vmulhu_vv_u16m1(__riscv_vreinterpret_v_u32m1_u16m1(index_a_c), __riscv_vreinterpret_v_u32m1_u16m1(const_vec_ac), vl);
-
-    // multiply 16-bit integers and store low 16 bits of 32-bit result
-    vuint16m1_t vec_shifted_bd = __riscv_vmul_vv_u16m1(__riscv_vreinterpret_v_u32m1_u16m1(index_b_d), __riscv_vreinterpret_v_u32m1_u16m1(const_vec_bd), vl);
-
-    vl = __riscv_vsetvlmax_e32m1();
-
-    return __riscv_vor_vv_u32m1(__riscv_vreinterpret_v_u16m1_u32m1(vec_shifted_ac), __riscv_vreinterpret_v_u16m1_u32m1(vec_shifted_bd), vl);
-}
-
-void base64_encode(uint8_t *restrict input, uint8_t *output, size_t length)
-{
-    size_t vl;
-
-    // size_t vlmax = __riscv_vsetvlmax_e32m1();
-
-    size_t vlmax_8 = __riscv_vsetvlmax_e8m1();
-
-    const vuint16m2_t vec_index = loadIndex();
-
-    vint8m1_t offset_vec = __riscv_vle8_v_i8m1(offsets, vlmax_8);
-
-    size_t input_slice = (vlmax_8 / 4) * 3;
-
-    for (; length >= input_slice; length -= input_slice)
-    {
-
-        vl = __riscv_vsetvl_e8m1(input_slice);
-
-        vuint8m1_t vec_input = __riscv_vle8_v_u8m1(input, vl);
-
-        vl = __riscv_vsetvl_e8m1(vlmax_8);
-
-        vuint8m1_t vec_gather = __riscv_vrgatherei16_vv_u8m1(vec_input, vec_index, vl);
-
-        vl = __riscv_vsetvlmax_e32m1();
-
-        // two different ways to compute lookup indices
-        // vuint32m1_t vec_lookup_indices = create_lookup_indices_naive(vec_gather, vl);
-        vuint32m1_t vec_lookup_indices = create_lookup_indices_opt(vec_gather, vl);
-
-        vl = __riscv_vsetvlmax_e8m1();
-
-        // three different ways to calculate the lookup step
-        // vuint8m1_t base64_chars = __riscv_vluxei8_v_u8m1(b64chars, __riscv_vreinterpret_v_u32m1_u8m1(vec_lookup_indices), vl);
-        // vuint8m1_t base64_chars = branchless_table_lookup_naive(__riscv_vreinterpret_v_u32m1_u8m1(vec_lookup_indices), vl);
-        vuint8m1_t base64_chars = branchless_table_lookup_opt(__riscv_vreinterpret_v_u32m1_u8m1(vec_lookup_indices), offset_vec, vl);
-
-        //  __riscv_vse8_v_u8m1(output, __riscv_vreinterpret_v_u32m1_u8m1(vec_lookup_indices), vl);
-        __riscv_vse8_v_u8m1(output, base64_chars, vl);
-
-        vl = __riscv_vsetvl_e8m1(length);
-        input += input_slice;
-        output += vlmax_8;
-    }
-    Base64encode((char *)output, (char *)input, length);
-}
-
-// #define N 1000
-#define N 1024 * 1024 * 30 // 32 MB
-
-char *setupInputData()
-{
-    char alphabet[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    char *inputData = (char *)malloc(sizeof(char) * N);
-
-    for (int i = 0; i < N; i++)
-    {
-        inputData[i] = alphabet[i % 26];
-    }
-
-    return inputData;
-}
-
-void checkResults(uint8_t *output_scalar, uint8_t *output_vector, size_t length)
+void checkResults(int8_t *output_scalar, int8_t *output_vector, size_t length)
 {
     size_t error = 0;
     for (int i = 0; i < length; i++)
     {
         if (output_scalar[i] != output_vector[i])
         {
-            printf("Error at index %d!\n", i);
+            printf("Error at index %d! scalar is 0x%02X, vector is 0x%02X \n", i, output_scalar[i], output_vector[i]);
             error = 1;
             break;
         }
@@ -182,64 +18,91 @@ void checkResults(uint8_t *output_scalar, uint8_t *output_vector, size_t length)
     }
 }
 
-// assembly function
-void base64_encode_asm(uint8_t *data, char *output, const int8_t *offsets, const uint16_t *index, int *length);
+int64_t timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
+{
+    return ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) -
+           ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec);
+}
+
+char *setupInputData(int N)
+{
+    // char alphabet[26] = "MTIzNDU2NysvQUJDREVGR0g=";
+    char alphabet[900] = "CgpIaSBldmVyeW9uZSwgaSByZWNlbnRseSBzdGFydGVkIHN0dWR5aW5nIHRoZSBSSVNDLVYgYXJjaGl0ZWN0dXJlLCBhbmQgbWFuYWdlZCB0byBtYWtlIG15IG93biAzMmJpdCB2ZXJzaW9uIGluIGEgZ2FtZSBjYWxsZWQgVHVyaW5nIGNvbXBsZXRlLiBUaGUgc3lzdGVtIGlzIGFibGUgdG8gZXhlY3V0ZSBldmVyeSBpbnN0cnVjdGlvbiBvZiB0aGUgYmFzZSBtb2R1bGVzLCBub3cgdGhhdCBpIHdhbnQgdG8gdHJ5IGFuZCBhZGQgc3VwcG9ydCBmb3IgZmxvYXRpbmcgcG9pbnQgbnVtYmVycywgaSdtIHN0dWNrIHdpdGggYSByZWFsbHkgc3R1cGlkIHF1ZXN0aW9uPwoKSSBhZGRlZCAzMiBzZXBhcmF0ZSByZWdpc3RlcnMgZm9yIHN0b3JpbmcgZmxvYXRzLCBhbmQgYW4gZW5jb2RlciBmb3IgdGhlIElFRUUtNzU0IGZvcm1hdC4gYnV0IGlmIGkgdXNlIHNvbWV0aGluZyBsaWtlCgpsaSB0MCwgNjU0MzIxCgpmY3Z0LnMudyBmdDAsIHQwCgpmdDAgd2lsbCBiZSBzZXQgdG8gNjU0MzIxLjAgKElFRUUgZW5jb2RlZCkKCkhlcmUgY29tZXMgdGhlIHN0dXBpZCBxdWVzdGlvbi4uLiBob3cgZG8gaSBwdXQgc3R1ZmYgYWZ0ZXIgdGhlIGRvdD8gZXZlcnkgbnVtYmVyIGkgY29udmVydCB3aWxsIGJlIGp1c3Qgbi4wCgpob3cgY2FuIGkgc2V0IGZ0MCB0byBzb21ldGhpbmcgbGlrZSAwLjYyIG9yIDEuND8=";
+
+    char *inputData = (char *)malloc(sizeof(char) * N);
+
+    for (int i = 0; i < N; i++)
+    {
+        inputData[i] = alphabet[i % 900];
+    }
+
+    return inputData;
+}
+
+#define RUN_SIZE 1
 
 int main(void)
 {
-
-    printf("Hello World\n");
-
     struct timespec start, end;
-    uint64_t timeElapsed_scalar, timeElapsed_vector;
+    uint64_t timeElapsed_scalar;
 
-    char *inputData = setupInputData();
+    // const char *base64_data = "QUJDREVGR2FiY2RlZmcxMjM0NTY3";
+    // const char *base64_data = "MTIzNDU2NysvQUJDREVGR0hhYmNkZWZnaGlqa2w=";
 
-    int encode_length = Base64encode_len(N * sizeof(char));
+    // const int a[run_size] = {2048, 4096, 8192, 1000000, 2000000, 4000000, 8000000, 16000000, 32000000, 64000000};
+    const int a[RUN_SIZE] = {900};
 
-    uint8_t *output_scalar = (uint8_t *)malloc(sizeof(uint8_t) * encode_length);
-    uint8_t *output_vector = (uint8_t *)malloc(sizeof(uint8_t) * encode_length);
-
-    Base64encode((char *)output_scalar, inputData, N);
-
-    // measure scalar code
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    Base64encode((char *)output_scalar, inputData, N);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    timeElapsed_scalar = timespecDiff(&end, &start);
-    printf("base64_scalar time: %ld\n", timeElapsed_scalar / 1000000);
-
-    base64_encode((uint8_t *)inputData, output_vector, N);
-    // base64_encode_asm((uint8_t *)inputData, (char *)output_vector, offsets, gather_index_lmul4, &length);
-
-    // measure vector code
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    base64_encode((uint8_t *)inputData, output_vector, N);
-    // base64_encode_asm((uint8_t *)inputData, (char *)output_vector, offsets, gather_index_lmul4, &length);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    timeElapsed_vector = timespecDiff(&end, &start);
-    printf("base64_vector time: %ld\n", timeElapsed_vector / 1000000);
-
-    // float speedup = ((float)timeElapsed_scalar / (float)timeElapsed_vector);
-    // printf("speedup %.02f %%\n", speedup * 100);
-
-    for (int i = 0; i < 70; i++)
+    for (int i = 0; i < RUN_SIZE; i++)
     {
-        printf("%c", output_scalar[i]);
+
+        int N = a[i];
+
+        printf("running with %d bytes\n", N);
+
+        char *base64_data = setupInputData(N);
+
+        // input
+        for (int i = 0; i < 70; i++)
+        {
+            printf("%c ", base64_data[i]);
+        }
+        printf("\n\n");
+
+        size_t output_length = 0;
+        size_t output_length_rvv = 0;
+
+        int8_t *output_scalar = (int8_t *)malloc(N * sizeof(uint8_t));
+        int8_t *output_rvv = (int8_t *)malloc(N * sizeof(uint8_t));
+
+        build_decoding_table();
+
+        output_scalar = (int8_t *)base64_decode((const unsigned char *)base64_data, N, &output_length);
+        // unsigned char *decoded = base64_decode(base64_data, 28, &output_length);
+
+        // measure scalar code
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        output_scalar = (int8_t *)base64_decode((const unsigned char *)base64_data, N, &output_length);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        timeElapsed_scalar = timespecDiff(&end, &start);
+        printf("base64_scalar time (qs): %ld\n", timeElapsed_scalar / 1);
+
+        base64_decode_rvv(base64_data, output_rvv, N, &output_length_rvv);
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        base64_decode_rvv(base64_data, output_rvv, N, &output_length_rvv);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        timeElapsed_scalar = timespecDiff(&end, &start);
+        printf("base64_rvv time (qs): %ld\n", timeElapsed_scalar / 1);
+
+        printf("--------------------\n");
+
+        checkResults(output_scalar, output_rvv, (N / 4) * 3);
+
+        free(base64_data);
+        free(output_rvv);
+        free(output_scalar);
     }
-    printf("\n");
 
-    for (int i = 0; i < 70; i++)
-    {
-        printf("%c", output_vector[i]);
-    }
-    printf("\n");
-
-    checkResults(output_scalar, output_vector, encode_length);
-
-    free(inputData);
-    free(output_scalar);
-    free(output_vector);
-
+    base64_cleanup();
     return 0;
 }
