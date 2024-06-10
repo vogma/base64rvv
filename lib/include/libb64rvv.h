ef5dba64c7d7c9190466bc1cfacf016817a4a2c8
vogma <vogel.marco@mailbox.org>
2024-06-10 19:59:07 +0200

    added picobase

--- lib/include/libb64rvv.h
+++ lib/include/libb64rvv.h
@@ -10,10 +10,13 @@
 // scalar code
 int Base64encode(char *encoded, const char *string, int len);
 unsigned char *base64_decode(const unsigned char *data, size_t input_length, unsigned char *output_data, size_t *output_length);
+unsigned int base64_decode_tail(const char *in, unsigned int inlen, unsigned char *out);
 
 // vectorized code
 void base64_encode_rvv(uint8_t *restrict input, uint8_t *output, size_t length);
-void base64_decode_rvv(const char *data, int8_t *output, size_t input_length, size_t *output_length);
+size_t base64_decode_rvv(const char *data, int8_t *output, size_t input_length);
+
+size_t DecodeChunk(const char *in, size_t inLen,uint8_t *out);
 
 int Base64encode_len(int len);
 
