ff9caf281e276115945298dc1ff966e702624b2a
vogma <vogel.marco@mailbox.org>
2024-06-18 17:23:36 +0200

    implemented m4 decode

--- lib/include/libb64rvv.h
+++ lib/include/libb64rvv.h
@@ -2,7 +2,8 @@
 #define LIBB64RVV_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "stdio.h"
@@ -11,17 +12,18 @@ extern "C" {
 #include "riscv_vector.h"
 #include <time.h>
 
-// scalar code
-int Base64encode(char *encoded, const char *string, int len);
-unsigned int base64_decode_tail(const char *in, unsigned int inlen, unsigned char *out);
+    // scalar code
+    int Base64encode(char *encoded, const char *string, int len);
+    unsigned int base64_decode_tail(const char *in, unsigned int inlen, unsigned char *out);
 
-// vectorized code
-void base64_encode_rvv(uint8_t * input, uint8_t *output, size_t length);
-void base64_decode_rvv(const char *data, int8_t *output, size_t input_length);
+    // vectorized code
+    void base64_encode_rvv(uint8_t *input, uint8_t *output, size_t length);
+    void base64_decode_rvv(const char *data, int8_t *output, size_t input_length);
+    void base64_decode_rvv_m4(const char *data, int8_t *output, size_t input_length);
 
-// size_t DecodeChunk(const char *in, size_t inLen,uint8_t *out);
+    // size_t DecodeChunk(const char *in, size_t inLen,uint8_t *out);
 
-int Base64encode_len(int len);
+    int Base64encode_len(int len);
 
 #ifdef __cplusplus
 }
