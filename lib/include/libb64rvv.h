a018fa5478155eb6017ed28ffecfdac8e8807854
vogma <vogel.marco@mailbox.org>
2024-06-20 17:16:15 +0200

    added new function signature

--- lib/include/libb64rvv.h
+++ lib/include/libb64rvv.h
@@ -18,6 +18,7 @@ extern "C"
 
     // vectorized code
     void base64_encode_rvv(uint8_t *input, uint8_t *output, size_t length);
+    size_t base64_decode_rvv_m1(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m2(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m4(const char *data, int8_t *output, size_t input_length);
 
