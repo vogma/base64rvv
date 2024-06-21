896cf976cd80982e6b342b3baf18c40e4a0f1b50
vogma <vogel.marco@mailbox.org>
2024-06-21 09:04:59 +0200

    added method signature

--- lib/include/libb64rvv.h
+++ lib/include/libb64rvv.h
@@ -19,6 +19,7 @@ extern "C"
     // vectorized code
     void base64_encode_rvv_m4(uint8_t *input, uint8_t *output, size_t length);
     void base64_encode_rvv_m2(uint8_t *input, uint8_t *output, size_t length);
+    void base64_encode_rvv_m1(uint8_t *input, uint8_t *output, size_t length);
     size_t base64_decode_rvv_m1(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m2(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m4(const char *data, int8_t *output, size_t input_length);
