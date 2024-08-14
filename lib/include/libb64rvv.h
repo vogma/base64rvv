88faa9cc27b2ad666df81e18e9c872a794a57e86
vogma <vogel.marco@mailbox.org>
2024-08-14 18:28:03 +0200

    implemented naive m2 decode

--- lib/include/libb64rvv.h
+++ lib/include/libb64rvv.h
@@ -16,7 +16,8 @@ extern "C"
 #define BASE64DE_FIRST '+'
 #define BASE64DE_LAST 'z'
 
-    static const uint8_t index_decode[32] = {2, 1, 0, 6, 5, 4, 10, 9, 8, 14, 13, 12, 18, 17, 16, 22, 21, 20, 26, 25, 24, 30, 29, 28, 15, 3, 7, 11, 19, 23, 27, 31};
+    // static const uint8_t index_decode[32] = {2, 1, 0, 6, 5, 4, 10, 9, 8, 14, 13, 12, 18, 17, 16, 22, 21, 20, 26, 25, 24, 30, 29, 28, 15, 0, 0, 0, 0, 0, 0, 0};
+    static const uint8_t index_decode[] = {2,1,0,6,5,4,10,9,8,14,13,12,18,17,16,22,21,20,26,25,24,30,29,28,34,33,32,38,37,36,42,41,40,46,45,44,50,49,48,54,53,52,58,57,56,62,61,60,66,65,64,70,69,68,74,73,72,78,77,76,82,81,80,86,85,84,90,89,88,94,93,92,98,97,96,102,101,100,106,105,104,110,109,108,114,113,112,118,117,116,122,121,120,126,125,124};
     static const int8_t shift_lut[16] = {
         /* 0 */ 0x00, /* 1 */ 0x00, /* 2 */ 0x3e - 0x2b, /* 3 */ 0x34 - 0x30,
         /* 4 */ 0x00 - 0x41, /* 5 */ 0x0f - 0x50, /* 6 */ 0x1a - 0x61, /* 7 */ 0x29 - 0x70,
@@ -42,6 +43,7 @@ extern "C"
     void base64_encode_rvv_m1(uint8_t *input, uint8_t *output, size_t length);
     size_t base64_decode_rvv_m1(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m2(const char *data, int8_t *output, size_t input_length);
+    size_t base64_decode_rvv_m2_naive(const char *data, int8_t *output, size_t input_length);
     size_t base64_decode_rvv_m4(const char *data, int8_t *output, size_t input_length);
 
     size_t b64_decode_rvv(const char *src, char *dst, size_t length);
