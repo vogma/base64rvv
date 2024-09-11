da74ab39ff2922230b8804f6db6065ddaa949df3
vogma <vogel.marco@mailbox.org>
2024-09-11 09:34:26 +0200

    header

--- include/base64.h
+++ include/base64.h
@@ -52,9 +52,3 @@ void checkResults(uint8_t *output_scalar, uint8_t *output_vector, size_t length)
         printf("No Errors\n");
     }
 }
-
-int64_t timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
-{
-    return ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) -
-           ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec);
-}
